d8831a5dc410b0ced1da28f129d8db3f340f702c
Дмитрий <rabolan@mail.ru>
2019-04-22 12:53:12 +0300

    раб с формами.

---
+++ CrmUi/Catalog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrmUi
+{
+    public partial class Catalog : Form
+    {
+        public Catalog()
+        {
+            InitializeComponent();
+        }
+
+        //Конструктор. При загрузке формы выводим необходиммые данные
+        public Catalog(DbSet db)
+        {
+           // dataGridView.DataBindings = db.Local.ToBindingList()
+        }
+
+        private void Catalog_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CrmUi/Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrmBl;
+using CrmBl.Model;
+
+namespace CrmUi
+{
+    public partial class Main : Form
+    {
+        //связь с бд
+        CrmContext db ;
+       // CrmBl.Model.CrmContext
+
+        public Main()
+        {
+            InitializeComponent();
+            db = new CrmContext(); // создаем конкретный обьект
+        }
+
+        //при загрузке формы
+        private void Main_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        //обьект меню
+        private void СущностиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var catalogProduct = new Catalog();
+        }
+    }
+}
