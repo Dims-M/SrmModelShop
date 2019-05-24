de7da52f40248693df7768823006f15180c3350b
Дмитрий <rabolan@mail.ru>
2019-05-25 00:30:55 +0300

    111

--- WFormsApp_GodeBloc/WFormsApp_GodeBloc/Form1.Designer.cs
+++ WFormsApp_GodeBloc/WFormsApp_GodeBloc/Form1.Designer.cs
@@ -37,14 +37,19 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 350);
+            this.button1.Location = new System.Drawing.Point(9, 284);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "ОК";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,9 +57,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(543, 315);
+            this.button2.Location = new System.Drawing.Point(407, 256);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(56, 19);
             this.button2.TabIndex = 1;
             this.button2.Text = "Выход";
             this.button2.UseVisualStyleBackColor = true;
@@ -63,17 +69,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 20);
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 17);
+            this.label1.Size = new System.Drawing.Size(26, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Лог";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(93, 350);
+            this.button3.Location = new System.Drawing.Point(70, 284);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.Size = new System.Drawing.Size(90, 19);
             this.button3.TabIndex = 3;
             this.button3.Text = "Прочитать лог";
             this.button3.UseVisualStyleBackColor = true;
@@ -82,17 +90,19 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(560, 20);
+            this.label2.Location = new System.Drawing.Point(420, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(18, 17);
+            this.label2.Size = new System.Drawing.Size(15, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Л";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(228, 350);
+            this.button4.Location = new System.Drawing.Point(171, 284);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(151, 23);
+            this.button4.Size = new System.Drawing.Size(113, 19);
             this.button4.TabIndex = 5;
             this.button4.Text = "Получить ";
             this.button4.UseVisualStyleBackColor = true;
@@ -106,34 +116,85 @@
             this.panel1.AutoScroll = true;
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(9, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(595, 297);
+            this.panel1.Size = new System.Drawing.Size(446, 200);
             this.panel1.TabIndex = 6;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(385, 351);
+            this.textBox1.Location = new System.Drawing.Point(289, 285);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(67, 22);
+            this.textBox1.Size = new System.Drawing.Size(51, 20);
             this.textBox1.TabIndex = 7;
             this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(467, 351);
+            this.button5.Location = new System.Drawing.Point(350, 285);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(90, 23);
+            this.button5.Size = new System.Drawing.Size(68, 19);
             this.button5.TabIndex = 8;
             this.button5.Text = "Очистить";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.Button5_Click);
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(9, 214);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 19);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Открыть смену";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(9, 237);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(110, 19);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Закрыть смену";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(246, 214);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(110, 19);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Продажа";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(140, 254);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(110, 22);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "Отменить чек";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(630, 385);
+            this.ClientSize = new System.Drawing.Size(472, 313);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.panel1);
@@ -141,6 +202,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Тест";
             this.panel1.ResumeLayout(false);
@@ -161,6 +223,10 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
 
--- WFormsApp_GodeBloc/WFormsApp_GodeBloc/kkt.cs
+++ WFormsApp_GodeBloc/WFormsApp_GodeBloc/kkt.cs
@@ -25,7 +25,7 @@ namespace WFormsApp_GodeBloc
             string settings = String.Format("{\"{0}\": {1}, \"{2}\": {3}, \"{4}\": \"{5}\", \"{6}\": {7}}",
             Constants.LIBFPTR_SETTING_MODEL, Constants.LIBFPTR_MODEL_ATOL_AUTO,
             Constants.LIBFPTR_SETTING_PORT, Constants.LIBFPTR_PORT_COM,
-            Constants.LIBFPTR_SETTING_COM_FILE, "COM39",
+            Constants.LIBFPTR_SETTING_COM_FILE, "COM4",
             Constants.LIBFPTR_SETTING_BAUDRATE, Constants.LIBFPTR_PORT_BR_115200);
 
             fptr.setSettings(settings);
@@ -50,10 +50,293 @@ namespace WFormsApp_GodeBloc
             return temp;
 
         }
-
-        public void ZaprosInfu()
+        //Заспрос ккм имя и завотской номер
+        public string ZaprosInfu()
         {
-            queryData()
+            IFptr fptr = new Fptr();
+            fptr.open(); // открытие файла.
+
+            fptr.setParam(Constants.LIBFPTR_PARAM_DATA_TYPE, Constants.LIBFPTR_DT_STATUS); // получение всех параметров
+            fptr.queryData();
+
+            string model = fptr.getParamInt(Constants.LIBFPTR_PARAM_MODEL).ToString(); //получение модели
+            string smena  = fptr.getParamInt(Constants.LIBFPTR_PARAM_SHIFT_STATE).ToString();
+            string numberKKT =  fptr.getParamString(Constants.LIBFPTR_PARAM_SERIAL_NUMBER);
+            string nameKKM = fptr.getParamString(Constants.LIBFPTR_PARAM_MODEL_NAME); //имя ккм
+
+            
+
+            return $"***ИНФОРМАЦИЯ О ККТ*** \t\n Имя ККМ:{nameKKM} \t\n Серийный номер:{numberKKT} \t\n Состояние текущей смены: {smena} ";
         }
+        /// <summary>
+        /// Получение суммы
+        /// </summary>
+        /// <returns></returns>
+        public string GetSummaKeshDA()
+        {
+            IFptr fptr = new Fptr();
+            fptr.open(); // открытие файла.
+
+            fptr.setParam(Constants.LIBFPTR_PARAM_DATA_TYPE, Constants.LIBFPTR_DT_CASH_SUM); // получение суммы ккт
+            fptr.queryData();
+                 
+            string getSumma = fptr.getParamDouble(Constants.LIBFPTR_PARAM_SUM).ToString();
+
+            return getSumma;
+        }
+
+
+       public string CountRegestracia()
+        {
+            IFptr fptr = new Fptr();
+            fptr.open(); // открытие файла.
+
+            fptr.setParam(Constants.LIBFPTR_PARAM_DATA_TYPE, Constants.LIBFPTR_DT_REGISTRATIONS_COUNT); // получение количества регистраций
+            fptr.queryData();
+
+            string Getcount = fptr.getParamInt(Constants.LIBFPTR_PARAM_COUNT).ToString(); // запрос к кассе
+
+            return Getcount;
+        }
+
+        //Запрос суммы выплат
+        public string GetCashCount()
+        {
+            IFptr fptr = new Fptr();
+            fptr.open(); // открытие файла.
+
+            fptr.setParam(Constants.LIBFPTR_PARAM_DATA_TYPE, Constants.LIBFPTR_DT_REVENUE); // получение количества регистраций
+            fptr.queryData();
+
+            string getCashCount = fptr.getParamDouble(Constants.LIBFPTR_PARAM_SUM).ToString();
+            return getCashCount;
+        }
+
+       //количество чеков
+       public string GetChekCaunt()
+        {
+            IFptr fptr = new Fptr();
+            fptr.open(); // открытие файла.
+
+            fptr.setParam(Constants.LIBFPTR_PARAM_DATA_TYPE, Constants.LIBFPTR_PARAM_RECEIPT_TYPE); // получение количества регистраций
+            fptr.setParam(Constants.LIBFPTR_PARAM_RECEIPT_TYPE, Constants.LIBFPTR_RT_SELL); //rjkbxtcndj чеков прихода
+            fptr.queryData();
+
+            string countChek = fptr.getParamInt(Constants.LIBFPTR_PARAM_DOCUMENTS_COUNT).ToString();// получение количества чеков
+
+            return countChek;
+
+        }
+
+        public string TimeDateKKM()
+        {
+            IFptr fptr = new Fptr();
+            fptr.open(); // открытие файла.
+           
+            fptr.setParam(Constants.LIBFPTR_PARAM_DATA_TYPE, Constants.LIBFPTR_DT_DEVICE_UPTIME); // получение количества регистраций
+
+            string timeKKM = fptr.getParamInt(Constants.LIBFPTR_PARAM_DEVICE_UPTIME).ToString();
+
+            return timeKKM;
+        }
+
+        ////*****************
+        /// <summary>
+        /// Открытие смены
+        /// </summary>
+        public string RegestraciAOpenSmena()
+        {
+            IFptr fptr = new Fptr();
+            fptr.open(); // открытие файла.
+
+            //fptr.setParam(1021, "Кассир Иванов И.");
+            //fptr.setParam(1203, "123456789047");
+            //fptr.operatorLogin(); // регистрация, применение настроек
+
+         //  Открытие смены
+
+            fptr.setParam(1021, "Кассир Иванов И.");
+            fptr.setParam(1203, "123456789047");
+            fptr.operatorLogin();
+
+           int open = fptr.openShift(); // открытие смены
+
+            string nomerErrot = fptr.errorCode().ToString();
+            string opisaniError = fptr.errorDescription();
+
+            fptr.checkDocumentClosed(); // проверка что операция открытия смены успешно проведена
+
+
+            fptr.close();
+            return $"Состояние операции. Номер ошибки {nomerErrot}| Описание ошибки{opisaniError}";
+        }
+
+        /// <summary>
+        /// Закрыть смену
+        /// </summary>
+        /// <returns></returns>
+        public string CloseSmena()
+        {
+            IFptr fptr = new Fptr();
+            fptr.open(); // открытие файла.
+
+            fptr.setParam(1021, "Кассир Иванов И.");
+            fptr.setParam(1203, "123456789047");
+            fptr.operatorLogin();
+
+            fptr.setParam(Constants.LIBFPTR_PARAM_REPORT_TYPE, Constants.LIBFPTR_RT_CLOSE_SHIFT); // параметры закрытия смены
+            fptr.report();// сам факт закрыия смены
+
+            string nomerErrot = fptr.errorCode().ToString();
+            string opisaniError = fptr.errorDescription();
+
+            fptr.checkDocumentClosed();
+            fptr.close(); // закрытие драйвера
+
+            return $"Состояние операции. Номер ошибки {nomerErrot}| Описание ошибки{opisaniError}";
+        }
+
+
+        public string CloseCheks()
+        {
+            IFptr fptr = new Fptr();
+            fptr.open(); // открытие файла.
+
+            fptr.cancelReceipt();
+            string nomerErrot = fptr.errorCode().ToString();
+            string opisaniError = fptr.errorDescription();
+
+            fptr.close(); // закрытие драйвера
+
+            return $"Состояние операции. Номер ошибки {nomerErrot}| Описание ошибки{opisaniError}";
+            fptr.close(); // закрытие драйвера
+        }
+
+        public string OpenChekTreid()
+        {
+            IFptr fptr = new Fptr();
+            fptr.open(); // открытие файла.
+
+            fptr.setParam(1021, "Кассир Иванов И.");
+            fptr.setParam(1203, "123456789047");
+            fptr.operatorLogin();
+
+            fptr.setParam(Constants.LIBFPTR_PARAM_RECEIPT_TYPE, Constants.LIBFPTR_RT_SELL); // параметр вида чека. Тут стоит чек продажи
+            fptr.openReceipt(); // метод открыть чек!!
+
+            string nomerErrot = fptr.errorCode().ToString();
+            string opisaniError = fptr.errorDescription();
+
+         //   label1.Text += $"Состояние операции. Номер ошибки {nomerErrot}| Описание ошибки{opisaniError}\t\n";
+
+            //// Открытие чека возврата продажи с автоматическим расчетом НДС18
+
+            //fptr.setParam(1021, "Кассир Иванов И.");
+            //fptr.setParam(1203, "123456789047");
+            //fptr.operatorLogin();
+
+            //nomerErrot += fptr.errorCode().ToString();
+            //opisaniError += fptr.errorDescription();
+
+            //label1.Text += $"Состояние операции. Номер ошибки {nomerErrot}| Описание ошибки{opisaniError}\t\n";
+
+            //fptr.setParam(Constants.LIBFPTR_PARAM_RECEIPT_TYPE, Constants.LIBFPTR_RT_SELL_RETURN);
+            //fptr.setParam(Constants.LIBFPTR_PARAM_USE_VAT18, true);
+            //fptr.openReceipt();
+
+            //nomerErrot += fptr.errorCode().ToString();
+            //opisaniError += fptr.errorDescription();
+            //label1.Text += $"Состояние операции. Номер ошибки {nomerErrot}| Описание ошибки{opisaniError}\t\n";
+
+            //// Регистрация позиции с указанием суммы налога
+
+            fptr.setParam(Constants.LIBFPTR_PARAM_COMMODITY_NAME, "Тестовой Товар");
+            fptr.setParam(Constants.LIBFPTR_PARAM_PRICE, 100); // цена
+            fptr.setParam(Constants.LIBFPTR_PARAM_QUANTITY, 100); //количество товара
+            fptr.setParam(Constants.LIBFPTR_PARAM_TAX_TYPE, Constants.LIBFPTR_TAX_VAT0); // сумма ндс
+            // fptr.setParam(Constants.LIBFPTR_PARAM_TAX_TYPE, Constants.LIBFPTR_TAX_VAT18); // сумма ндс
+            // fptr.setParam(Constants.LIBFPTR_PARAM_TAX_SUM, 51.5);
+            fptr.setParam(Constants.LIBFPTR_PARAM_USE_ONLY_TAX_TYPE, true); //Регистрация позиции без расчета суммы налога
+
+            fptr.registration(); // принять.
+
+            //  Оплата чека
+
+            fptr.setParam(Constants.LIBFPTR_PARAM_PAYMENT_TYPE, Constants.LIBFPTR_PT_CASH); //вид оплаты Тут наликом
+            fptr.setParam(Constants.LIBFPTR_PARAM_PAYMENT_SUM, 1000.00);  // сумма
+            fptr.payment();  // плптеж
+
+            nomerErrot += fptr.errorCode().ToString();
+            opisaniError += fptr.errorDescription();
+           // label1.Text += $"Состояние операции. Номер ошибки {nomerErrot}| Описание ошибки{opisaniError}\t\n";
+
+            //// Регистрация налога на чек
+
+            //fptr.setParam(Constants.LIBFPTR_PARAM_TAX_TYPE, Constants.LIBFPTR_TAX_VAT18);
+            //fptr.setParam(Constants.LIBFPTR_PARAM_TAX_SUM, 100.00);
+            //fptr.receiptTax();
+
+            nomerErrot += fptr.errorCode().ToString();
+            opisaniError += fptr.errorDescription();
+          //  label1.Text += $"Состояние операции. Номер ошибки {nomerErrot}| Описание ошибки{opisaniError}\t\n";
+
+
+            //  Регистрация итога чека
+            fptr.setParam(Constants.LIBFPTR_PARAM_SUM, 1000.00); // итог сумма
+            fptr.receiptTotal(); // предварительный итог
+
+
+            // Закрытие полностью оплаченного чека
+
+            fptr.closeReceipt();
+            nomerErrot += fptr.errorCode().ToString();
+            opisaniError += fptr.errorDescription();
+           // label1.Text += $"Состояние операции. Номер ошибки {nomerErrot}| Описание ошибки{opisaniError}\t\n";
+
+
+            //  Проверка закрытия документа(на примере закрытия фискального чека)
+
+            fptr.closeReceipt();
+
+            //while (fptr.checkDocumentClosed() < 0)
+            //{
+            //    // Не удалось проверить состояние документа. Вывести пользователю текст ошибки, попросить устранить неполадку и повторить запрос
+            //    Console.WriteLine(fptr.errorDescription());
+            //    continue;
+            //}
+
+            //if (!fptr.getParamBool(Constants.LIBFPTR_PARAM_DOCUMENT_CLOSED))
+            //{
+            //    // Документ не закрылся. Требуется его отменить (если это чек) и сформировать заново
+            //    fptr.cancelReceipt();
+            //    return;
+            //}
+
+            //if (!fptr.getParamBool(Constants.LIBFPTR_PARAM_DOCUMENT_PRINTED))
+            //{
+            //    // Можно сразу вызвать метод допечатывания документа, он завершится с ошибкой, если это невозможно
+            //    while (fptr.continuePrint() < 0)
+            //    {
+            //        // Если не удалось допечатать документ - показать пользователю ошибку и попробовать еще раз.
+            //        Console.WriteLine(String.Format("Не удалось напечатать документ (Ошибка \"{0}\"). Устраните неполадку и повторите.", fptr.errorDescription()));
+            //        continue;
+            //    }
+
+
+            // Допечатывание документа
+
+            fptr.continuePrint();
+
+
+            nomerErrot += fptr.errorCode().ToString();
+            opisaniError += fptr.errorDescription();
+
+            fptr.close(); // закрытие драйвера
+
+            return $"Состояние операции. Номер ошибки {nomerErrot}| Описание ошибки{opisaniError}";
+        }
+
+        
+
     }
 }
