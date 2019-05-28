44e93faa2140208341b9433c531aa0f83dd16509
Дмитрий <rabolan@mail.ru>
2019-05-28 17:59:03 +0300

    123

---
+++ RaboraXML/RaboraXML/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RaboraXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        RabXml rabXml = new RabXml();
+
+        //Кнопка пуск
+        private void Button1_Click(object sender, EventArgs e)
+        {
+           label1.Text = rabXml.ReadingXml();
+
+        }
+    }
+}
---
+++ RaboraXML/RaboraXML/RabXml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RaboraXML
+{
+    /// <summary>
+    /// Логика работы с Xml файлами
+    /// </summary>
+    public class RabXml
+    {
+        /// <summary>
+        /// Чтение файла Xml
+        /// </summary>
+        public string ReadingXml()
+        {
+           // string patch = @"users.xml";
+            string patch = @"users2.xml";
+            string tempItem ="";
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(patch);
+
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+
+            // обход всех узлов в корневом элементе
+
+            foreach (XmlNode xnode in xRoot)
+            {
+
+                // получаем атрибут name
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("Header");
+                    if (attr != null)
+                        tempItem += $"ИМЯ узла{attr.Value}\t\n";
+                }
+                // обходим все дочерние узлы элемента user
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    // если узел - company
+                    if (childnode.Name == "company\t\n")
+                    {
+                        tempItem +=  $"Компания: {childnode.InnerText}\t\n";
+                    }
+                    // если узел age
+                    if (childnode.Name == "age\t\n")
+                    {
+                        tempItem += ($"Возраст: {childnode.InnerText}\t\n");
+                    }
+                }
+
+            }
+            return tempItem;
+        }
+
+
+    }
+}
